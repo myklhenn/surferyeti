c3c00eae804b5f6ff87a8a5df081193b24b0ba1d
Michael Henning <mykl951@gmail.com>
2017-05-04 14:49:49 -0700

    Changed to better scrolling plugin

--- js/yeti.js
+++ js/yeti.js
@@ -1,18 +1,27 @@
-$(function () {
-   $('body').mousewheel(function (event) {
-      console.log(event.deltaX, event.deltaY, event.deltaFactor);
 
-      $('#parallax').animate(
-         {
-            left: '-=' + (event.deltaY * 10)
-         },
-         {
-            duration: 500,
-            easing: 'swing',
-            queue: false,
-      }, 'swing');
-
-      // this.scrollLeft -= (event.deltaY);
-      event.preventDefault();
+$(function ($) {
+   $.jInvertScroll(['#parallax'], {    // an array containing the selector(s) for the elements you want to animate
+      onScroll: function (percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+         // console.log(percent);
+      }
    });
 });
+
+// $(function () {
+//    $('body').mousewheel(function (event) {
+//       console.log(event.deltaX, event.deltaY, event.deltaFactor);
+
+//       $('#parallax').animate(
+//          {
+//             left: '-=' + (event.deltaY * 60)
+//          },
+//          {
+//             duration: 500,
+//             easing: 'swing',
+//             queue: false,
+//       }, 'swing');
+
+//       // this.scrollLeft -= (event.deltaY);
+//       event.preventDefault();
+//    });
+// });
