26152cb4a6f82cf16257f2b8d2b785510521e284
Michael Henning <mykl951@gmail.com>
2017-05-11 23:45:45 -0700

    Scrolling to the sections and resizing works now!

--- js/yeti.js
+++ js/yeti.js
@@ -1,27 +1,31 @@
-
 $(function ($) {
    $.jInvertScroll(['#parallax'], {    // an array containing the selector(s) for the elements you want to animate
-      onScroll: function (percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+      onScroll: function (percent) {   // optional: callback function that will be called when the user scrolls down, 
+                                       // useful for animating other things on the page
          // console.log(percent);
       }
    });
+
+   $('#page-links a').on('click', function (e) {
+      e.preventDefault();
+
+      var target = '.'+$(this).data('target');
+      var left = $(target).position().left;
+      var total = $('#parallax').width();
+      var perc = left / (total - $(window).width()) * 100;
+      
+      var height = $('html').height();
+      var final = ((height - $(window).height())/ 100 * perc);
+      
+      $('html, body').animate({scrollTop: final}, 1000);
+   });
+
+   $(document).ready(function () {
+      $('#parallax').css("height", $(window).height() + "px");
+   });
+
+   $(window).resize(function () {
+      $('#parallax').css("height",$(window).height()+"px");
+   });
+
 });
-
-// $(function () {
-//    $('body').mousewheel(function (event) {
-//       console.log(event.deltaX, event.deltaY, event.deltaFactor);
-
-//       $('#parallax').animate(
-//          {
-//             left: '-=' + (event.deltaY * 60)
-//          },
-//          {
-//             duration: 500,
-//             easing: 'swing',
-//             queue: false,
-//       }, 'swing');
-
-//       // this.scrollLeft -= (event.deltaY);
-//       event.preventDefault();
-//    });
-// });
