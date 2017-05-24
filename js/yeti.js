81a753c92fb88c5bbc0c37048c08dcdf2eb1e55a
Michael Henning <mykl951@gmail.com>
2017-05-23 19:24:09 -0700

    Forgot to hit save on yeti.js

--- js/yeti.js
+++ js/yeti.js
@@ -1,36 +1,11 @@
 $(function ($) {
 
-   // window.fbAsyncInit = function() {
-   //    FB.init({
-   //       appId            : 'your-app-id',
-   //       autoLogAppEvents : true,
-   //       xfbml            : true,
-   //       version          : 'v2.9'
-   //    });
-   //    FB.AppEvents.logPageView();
-   // };
-
-   // (function(d, s, id){
-   //    var js, fjs = d.getElementsByTagName(s)[0];
-   //    if (d.getElementById(id)) {return;}
-   //    js = d.createElement(s); js.id = id;
-   //    js.src = "//connect.facebook.net/en_US/sdk.js";
-   //    fjs.parentNode.insertBefore(js, fjs);
-   // }(document, 'script', 'facebook-jssdk'));
-
    $.jInvertScroll(['#parallax', '#bkg-container'], {    // an array containing the selector(s) for the elements you want to animate
       onScroll: function (percent) {   // optional: callback function that will be called when the user scrolls down, 
                                        // useful for animating other things on the page
          // console.log(percent);
       }
    });
-
-   // var accessToken;   
-   // FB.getLoginStatus(function(response) {
-   //    if (response.status === 'connected') {
-   //       accessToken = response.authResponse.accessToken;
-   //    } 
-   // });
 
    $('#page-links a').on('click', function (e) {
       e.preventDefault();
