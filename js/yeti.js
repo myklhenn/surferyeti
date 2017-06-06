0dc03f19962517a9dce1f278f3f4fe6a252c804d
Michael Henning <mykl951@gmail.com>
2017-06-05 17:18:15 -0700

    Resizing handled, Snapcode overlay added, etc.

--- js/yeti.js
+++ js/yeti.js
@@ -1,39 +1,37 @@
 $(function ($) {
 
-   $.jInvertScroll(['#parallax', '#bkg-container'], { // array of elements to animate
-      onScroll: function (percent) { // function that is called when the user scrolls
+   /* set splash and snapcode screens to window size */
+   $('#splash-screen').css("height", $(window).height() + "px");
+   $('#splash-screen').css("width", $(window).width() + "px");
+   $('#snapcode-screen').css("height", $(window).height() + "px");
+   $('#snapcode-screen').css("width", $(window).width() + "px");
+
+   $.jInvertScroll(['#parallax', '#bkg-container'], { /* array of elements to animate */
+      onScroll: function (percent) { /* function that is called when the user scrolls */
          
          var total     = $('#parallax').width();
          var winWidth  = $(window).width();         
          var showsPerc = $('#shows-section').position().left / (total - winWidth);
-         var artPerc   = $('#photos-section').position().left / (total - winWidth);
+         var artPerc   = $('#info-section').position().left / (total - winWidth);
 
-         $('#bios-btn').css("color", "#dabc5b");
-         $('#shows-btn').css("color", "#dabc5b");
-         $('#photos-btn').css("color", "#dabc5b");
+         /* un-highlight all buttons */
+         $('#bios-btn').css("color", "#b75a01");
+         $('#shows-btn').css("color", "#b75a01");
+         $('#info-btn').css("color", "#b75a01");   
 
          if (percent >= artPerc-0.05) {
-            // highlight 'art' button
-            $('#photos-btn').css("color", "#b75a01");
-            // un-highlight other buttons
-            // $('#bios-btn').css("color", "#dabc5b");
-            // $('#shows-btn').css("color", "#dabc5b");
+            /* highlight 'info' button */
+            $('#info-btn').css("color", "#dabc5b");
          }
          else if (percent >= showsPerc-0.05) {
-            // highlight 'shows' button
-            $('#shows-btn').css("color", "#b75a01");
-            // un-highlight other buttons
-            // $('#bios-btn').css("color", "#dabc5b");
-            // $('#photos-btn').css("color", "#dabc5b");
+            /* highlight 'shows' button */
+            $('#shows-btn').css("color", "#dabc5b");
          }
          else { 
-            // highlight 'home' button
-            $('#bios-btn').css("color", "#b75a01");
-            // un-highlight other buttons
-            // $('#shows-btn').css("color", "#dabc5b");
-            // $('#photos-btn').css("color", "#dabc5b");
+            /* highlight 'home' button */
+            $('#bios-btn').css("color", "#dabc5b");
+            $('#logo-hover').fadeTo(200, 1);
          }
-
       }
    });
 
@@ -51,24 +49,45 @@ $(function ($) {
       $('html, body').animate({scrollTop: final}, 1000);
    });
 
+   $(window).on("load", function () {
+      /* reveal the page once everything (all images) is loaded */
+      $('#splash-screen').fadeTo(300, 0, function () {
+         $('#splash-screen').css("display", "none");
+         $('#splash-logo').css("display", "none");
+      });
+   });
+
    $(document).ready(function () {
       /* set locale of dates used in social-feed */
       moment.locale('en');
       
       /* set height of scrolling section to window height */
-      $('#parallax').css("height", $(window).height() + "px");
-      
+      // $('#parallax').css("height", $(window).height() + "px");
+
+      if ($(window).height() < 768) {
+         $('#bottom-border-gradient').css("top",($(window).height()-150)+"px");
+      }      
+      else {
+         $('#bottom-border-gradient').css("top",618+"px");
+      }
+      $('#bottom-border-gradient').css("width",$(window).width()+"px");      
+
       /* highlight the first ('home') nav button */
-      $('#bios-btn').css("color", "#b75a01");
+      $('#bios-btn').css("color", "#dabc5b");
+
+      $('#splash-screen').fadeTo(300, 0, function () {
+         $('#splash-screen').css("display", "none");
+         $('#splash-logo').css("display", "none");
+      });
 
       /* social-feed setup */
       $('.social-feed-container').socialfeed({
          facebook:{
             accounts: ['@surferyeti','!surferyeti'],
-            limit: 10, // max number of posts to load
+            limit: 10, /* max number of posts to load */
             access_token: "1936729116556277|bdf3f97141b8c115898421ada990e68b"
          },
-         length: 400, // for posts with text longer than this length, show an ellipsis
+         length: 400, /* for posts with text longer than this length, show an ellipsis */
          update_period: 5000,
          template: "fb-post-template.html",
          date_format: "ll",
@@ -77,9 +96,20 @@ $(function ($) {
    });
 
    $(window).resize(function () {
-      $('#parallax').css("height",$(window).height()+"px");
+      // $('#parallax').css("height", $(window).height() + "px");
+      
+      $('#splash-screen').css("height", $(window).height() + "px");
+      $('#splash-screen').css("width", $(window).width() + "px");
       // $('#bios-section').css("width", $(window).width() + "px");
       // $('.bio-bkg').css("width",$(window).width()+"px");
+
+      if ($(window).height() < 768) {
+         $('#bottom-border-gradient').css("top",($(window).height()-150)+"px");
+      }      
+      else {
+         $('#bottom-border-gradient').css("top",618+"px");
+      }
+      $('#bottom-border-gradient').css("width", $(window).width() + "px");
    });
    
    $('#sean-hover-box').hover(
@@ -173,7 +203,7 @@ $(function ($) {
    $('#social-hover-box').hover(
       function () { /* hover enter */
          /* extend height of hover trigger box */
-         $('#social-hover-box').css("height", "240px");
+         $('#social-hover-box').css("height", "180px");
          /* hide menu (three dots) button */
          $('#social-menu-btn').fadeTo(200, 0, function () {
             $('#social-menu-btn').css("visibility", "hidden");
@@ -181,18 +211,21 @@ $(function ($) {
          /* fade in and animate buttons */
          $('.social-btn').css("visibility", "visible");
          $('.social-btn').fadeTo(200, 1);
+
          $('#bandcamp-btn').css("transform",         "translate3d(0px, -60px, 0px)");
          $('#bandcamp-btn').css("-ms-transform",     "translate3d(0px, -60px, 0px)");
          $('#bandcamp-btn').css("-moz-transform",    "translate3d(0px, -60px, 0px)");
          $('#bandcamp-btn').css("-webkit-transform", "translate3d(0px, -60px, 0px)");
-         $('#soundcloud-btn').css("transform",         "translate3d(0px, -120px, 0px)");
-         $('#soundcloud-btn').css("-ms-transform",     "translate3d(0px, -120px, 0px)");
-         $('#soundcloud-btn').css("-moz-transform",    "translate3d(0px, -120px, 0px)");
-         $('#soundcloud-btn').css("-webkit-transform", "translate3d(0px, -120px, 0px)");
-         $('#snapchat-btn').css("transform",         "translate3d(0px, -180px, 0px)");
-         $('#snapchat-btn').css("-ms-transform",     "translate3d(0px, -180px, 0px)");
-         $('#snapchat-btn').css("-moz-transform",    "translate3d(0px, -180px, 0px)");
-         $('#snapchat-btn').css("-webkit-transform", "translate3d(0px, -180px, 0px)");
+
+         $('#snapchat-btn').css("transform",         "translate3d(0px, -120px, 0px)");
+         $('#snapchat-btn').css("-ms-transform",     "translate3d(0px, -120px, 0px)");
+         $('#snapchat-btn').css("-moz-transform",    "translate3d(0px, -120px, 0px)");
+         $('#snapchat-btn').css("-webkit-transform", "translate3d(0px, -120px, 0px)");
+
+         // $('#soundcloud-btn').css("transform",         "translate3d(0px, -180px, 0px)");
+         // $('#soundcloud-btn').css("-ms-transform",     "translate3d(0px, -180px, 0px)");
+         // $('#soundcloud-btn').css("-moz-transform",    "translate3d(0px, -180px, 0px)");
+         // $('#soundcloud-btn').css("-webkit-transform", "translate3d(0px, -180px, 0px)");
       },
       function () { /* hover leave */
          /* restore original height of hover trigger box */
@@ -203,22 +236,52 @@ $(function ($) {
          /* fade out and animate buttons */
          $('.social-btn').fadeTo(200, 0, function () {
             $('.social-btn').css("visibility", "hidden");
-         });         
+         });    
+         
          $('#bandcamp-btn').css("transform",         "translate3d(0px, 0px, 0px)");
          $('#bandcamp-btn').css("-ms-transform",     "translate3d(0px, 0px, 0px)");
          $('#bandcamp-btn').css("-moz-transform",    "translate3d(0px, 0px, 0px)");
          $('#bandcamp-btn').css("-webkit-transform", "translate3d(0px, 0px, 0px)");
-         
-         $('#soundcloud-btn').css("transform",         "translate3d(0px, 0px, 0px)");
-         $('#soundcloud-btn').css("-ms-transform",     "translate3d(0px, 0px, 0px)");
-         $('#soundcloud-btn').css("-moz-transform",    "translate3d(0px, 0px, 0px)");
-         $('#soundcloud-btn').css("-webkit-transform", "translate3d(0px, 0px, 0px)");
-         
+
          $('#snapchat-btn').css("transform",         "translate3d(0px, 0px, 0px)");
          $('#snapchat-btn').css("-ms-transform",     "translate3d(0px, 0px, 0px)");
          $('#snapchat-btn').css("-moz-transform",    "translate3d(0px, 0px, 0px)");
          $('#snapchat-btn').css("-webkit-transform", "translate3d(0px, 0px, 0px)");
+         
+         // $('#soundcloud-btn').css("transform",         "translate3d(0px, 0px, 0px)");
+         // $('#soundcloud-btn').css("-ms-transform",     "translate3d(0px, 0px, 0px)");
+         // $('#soundcloud-btn').css("-moz-transform",    "translate3d(0px, 0px, 0px)");
+         // $('#soundcloud-btn').css("-webkit-transform", "translate3d(0px, 0px, 0px)");
       }
    );
    
+   $('#snapchat-btn').click(function () {
+      $('#snapcode-screen').css("display", "inline-block");
+      $('#snapcode, #snapcode-msg').css("display", "inline-block");
+      $('#snapcode-screen').fadeTo(400, 0.8, function () {
+         
+      });
+      $('#snapcode, #snapcode-msg').fadeTo(400, 1, function () {
+         
+      });
+   });
+
+   $('#snapcode, #snapcode-screen, #snapcode-msg').click(function () {
+      $('#snapcode-screen').fadeTo(400, 0, function () {
+         $('#snapcode-screen').css("display", "none");
+      });
+      $('#snapcode, #snapcode-msg').fadeTo(400, 0, function () {
+         $('#snapcode, #snapcode-msg').css("display", "none");
+      });
+   });
+   
+   $('#logo, #logo-hover').hover(
+      function () {
+         $('#logo-hover').fadeTo(200, 1);
+      },
+      function () {
+         $('#logo-hover').fadeTo(200, 0);
+      }
+   );
+
 });
